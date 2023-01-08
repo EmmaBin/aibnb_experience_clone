8ef316deef9fc04a74072a41389e08afc4606022
emmacodes <binma.emma@gmail.com>
2023-01-07 23:44:28 -0800

    display different badges using conditional rendering

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,19 @@ import star from '../assets/star.png'
 
 
 export default function Card(props){
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
     return(
         <div className="card">
-            {props.openSpots === 0 && <div className="card--badge">SOLD OUT</div>}
+            {/* if badge has value, it will display some message, sold our has its first 
+            priority to display, then online. If is has no badge, display nothing, which
+            means badge is falsy  */}
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            
             <img src={`public/images/${props.img}`} className='card--image' />
             <div className="card--stats">
                 <img src={star} className="card--star" />
