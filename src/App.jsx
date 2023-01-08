5f63ae6d10aa38d4d31c4af981930cb3f51ed387
emmacodes <binma.emma@gmail.com>
2023-01-07 23:06:20 -0800

    map data into components

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,30 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import cardsData from './data'
 
 function App() {
+  const cardsInfo = cardsData.map(data=>{
+
+    return <Card img ={data.coverImg}
+                  rating={data.stats.rating}
+                  reviewCount={data.stats.reviewCount}
+                  location={data.location}
+                  title={data.title}
+                  price={data.price}
+                  id={data.id}
+    />
+  })
  
 
   return (
     <>
     <Navbar />
     <Hero />
-    <Card />
+    <section className='card-list'>
+    {cardsInfo}
+    </section>
+    
     </>
     
   )
